1f18d4a9f89e3beca52dfd4e17e51d2cb54e09ac
Karlaa28 <karlacamino2017.28@gmail.com>
2023-09-28 20:59:33 -0500

    kv/1ercommit

---
+++ src/main/java/pe/edu/cibertec/DAWII_CL1_INVENTARIO/DawiiCl1InventarioApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL1_INVENTARIO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DawiiCl1InventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DawiiCl1InventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/DAWII_CL1_INVENTARIO/DawiiCl1InventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.DAWII_CL1_INVENTARIO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DawiiCl1InventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
