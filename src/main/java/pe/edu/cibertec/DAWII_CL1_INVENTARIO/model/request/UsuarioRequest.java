45f6530ce90de0f46afc65662af53e33ba3ef183
Karlaa28 <karlacamino2017.28@gmail.com>
2023-09-28 23:22:41 -0500

    paquetes

---
+++ src/main/java/pe/edu/cibertec/DAWII_CL1_INVENTARIO/model/request/UsuarioRequest.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.DAWII_CL1_INVENTARIO.model.request;
+
+public class UsuarioRequest {
+}
---
+++ src/main/java/pe/edu/cibertec/DAWII_CL1_INVENTARIO/repository/UsuarioRepository.java
@@ -0,0 +1,4 @@
+package pe.edu.cibertec.DAWII_CL1_INVENTARIO.repository;
+
+public interface UsuarioRepository {
+}
